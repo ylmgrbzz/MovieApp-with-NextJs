1ea9341b95c0bd3051ea122781baa6030632db55
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-31 11:23:40 +0300

    MovieAppNextJS

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const page = () => {
-  return <div>ylm</div>;
+  return (
+    <div>
+      {" "}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default page;
